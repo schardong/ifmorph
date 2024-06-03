c1ec739223e1aa61b97f0769e9db1c39448175f5
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2024-06-03 11:55:41 +0100

    Respecting the radius parameter now.

--- ifmorph/util.py
+++ ifmorph/util.py
@@ -321,8 +321,9 @@ def plot_landmarks(im: np.array, lm: np.array, c=(0, 255, 0), r=3) -> np.array:
         lmn[:, 1] *= imc.shape[1]
         lmn = lmn.astype(np.uint32)
 
+    rad = r//2
     for l in lmn:
-        imc[(l[0]-5):(l[0]+5), (l[1]-5):(l[1]+5), :] = c
+        imc[(l[0]-rad):(l[0]+rad), (l[1]-rad):(l[1]+rad), :] = c
 
     return imc
 
