160d86659dd21c51a067694527fbbf5f906ae6da
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2024-06-13 16:39:24 +0100

    Bumping version, fixing authorship and adding maintainer emails.

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ from setuptools import setup
 
 setup(
     name="ifmorph",
-    version="0.0.1",
+    version="1.0.0",
     description="Implementation of \"Neural Implicit Morphing of Face Images\".",
-    author="Guilherme Schardong, Tiago Novello",
+    author="Guilherme Schardong, Tiago Novello, Hallison Paz, Iurii Medvedev, Vinicius da Silva, Luiz Velho, Nuno Goncalves",
     packages=["ifmorph"],
+    maintainer_email="guilherme.schardong@isr.uc.pt,tiago.novello@impa.br",
+    license="mit"
 )
